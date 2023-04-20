7b57c593db51616cefe3c56ca6a2780fbb373e16
“SueldoMariano” <sueldo.011.ms@gmail.com>
2023-04-20 17:57:47 -0300

    Punto 1-1 realizado

---
+++ tp4_1_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int cantidadTareas;
+
+    printf("Ingrese la cantidad de tareas a cargar: ");
+    scanf("%d", &cantidadTareas);
+
+    printf("Se cargarán %d tareas.\n", cantidadTareas);
+
+    return 0;
+}
