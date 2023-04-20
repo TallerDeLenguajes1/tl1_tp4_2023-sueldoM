d98e7e6524008a2031c6659235d3930a010c5b7e
“SueldoMariano” <sueldo.011.ms@gmail.com>
2023-04-20 18:04:45 -0300

    Punto 1-1 realizado

--- tp4_1_1.c
+++ tp4_1_1.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_TAREAS 50
+
+struct Tarea {
+    int TareaID;
+    char *Descripcion;
+    int Duracion;
+};
+
+void cargar_tareas(struct Tarea *tareas, int *cant_tareas);
+void mostrar_tareas(struct Tarea *tareas, int cant_tareas);
 
 int main() {
-    int cantidadTareas;
-
-    printf("Ingrese la cantidad de tareas a cargar: ");
-    scanf("%d", &cantidadTareas);
-
-    printf("Se cargarán %d tareas.\n", cantidadTareas);
-
+    struct Tarea tareas[MAX_TAREAS];
+    int cant_tareas = 0;
     return 0;
 }
